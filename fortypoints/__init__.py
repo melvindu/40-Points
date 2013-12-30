99bbc5f3ced8261484e75f4e43a8fe19a927a3d9
Melvin Du <melvinclanceydu@gmail.com>
2013-12-29 18:17:44 -0800

    adding index page.

--- fortypoints/__init__.py
+++ fortypoints/__init__.py
@@ -1,2 +1,12 @@
-from flask import Flask
+import os
+
+from flask import Flask, g, session
+
+from config import secret_key
+
 app = Flask(__name__)
+app.secret_key = secret_key
+
+@app.route('/')
+def index():
+	return 'Forty Points'
