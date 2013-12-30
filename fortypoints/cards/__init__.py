fd06b4fc91472d44aa087f1fbd3c1c269e843141
Brian Louie <brian.y.louie@gmail.com>
2013-12-29 22:19:37 -0800

    Modified Cards directory
    Added inheritance; added constants.py file containing enums Suit and
    Number; assigned point value as attribute of Card object

--- fortypoints/cards/__init__.py
+++ fortypoints/cards/__init__.py
@@ -1,4 +1,10 @@
-class Card:
+class Card(object):
 	def __init__ (self, num, suit):
 		self.num = num
 		self.suit = suit
+		if num == 5:
+			self.points = 5
+		elif num == 10 or num == KING:
+			self.points = 10
+		else:
+			self.points = 0
---
+++ fortypoints/cards/constants.py
@@ -0,0 +1,21 @@
+class Suit(object):
+	HEART = 'heart'
+	SPADE = 'spade'
+	CLUB = 'club'
+	DIAMOND = 'diamond'
+
+class Number(object):
+	TWO = 2
+	THREE = 3
+	FOUR = 4
+	FIVE = 5
+	SIX = 6
+	SEVEN = 7
+	EIGHT = 8
+	NINE = 9
+	TEN = 10
+	JACK = 11
+	QUEEN = 12
+	KING = 13
+	SMALL_JOKER = 14
+	BIG_JOKER = 15
