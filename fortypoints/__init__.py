ee50c061d7ce8592f689929473675d9577a57bde
Melvin Du <melvinclanceydu@gmail.com>
2013-12-29 23:30:20 -0800

    adding loginmanager

--- fortypoints/__init__.py
+++ fortypoints/__init__.py
@@ -1,18 +1,20 @@
 import os
 
 from flask import Flask, g, session
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 from config import SECRET_KEY, SQLALCHEMY_DATABASE_URI
 
 db = SQLAlchemy()
-
+login_manager = LoginManager()
 
 def create_app():
   app = Flask(__name__)
   app.secret_key = SECRET_KEY
   app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
   db.init_app(app)
+  login_manager.init_app(app)
   return app
 
 app = create_app()
