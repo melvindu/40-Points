b73b7c3274a10b6dacc56985b59ccea1c77d7892
Melvin Du <melvinclanceydu@gmail.com>
2013-12-30 00:33:03 -0800

    adding status/role to user model

--- fortypoints/users/models.py
+++ fortypoints/users/models.py
@@ -1,9 +1,26 @@
 from fortypoints import db
 from fortypoints.models import ModelMixin
+from fortypoints.users import constants as USER
 
 class User(db.Model, ModelMixin):
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(80), unique=True)
-  email = db.Column(db.String(255), unique=True)
-  password = db.Column(db.String(255))
-  active = db.Column(db.Boolean())
+  id = db.Column(db.Integer(unsigned=True), primary_key=True)
+  name = db.Column(db.String(50), unique=True)
+  email = db.Column(db.String(120), unique=True)
+  password = db.Column(db.String(120))
+  role = db.Column(db.SmallInteger, default=USER.USER)
+  status = db.Column(db.SmallInteger, default=USER.NEW)
+
+      def __init__(self, name=None, email=None, password=None):
+        self.name = name
+        self.email = email
+        self.password = password
+
+
+      def get_status(self):
+        return USER.STATUS[self.status]
+
+      def get_role(self):
+        return USER.ROLE[self.role]
+
+      def __repr__(self):
+        return '<User {name}>'.format(name=self.name)
