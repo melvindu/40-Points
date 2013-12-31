3416e87e2193443a87094ea153fa5514f372c16e
Melvin Du <melvinclanceydu@gmail.com>
2013-12-30 23:00:12 -0800

    fix players

--- fortypoints/players/models.py
+++ fortypoints/players/models.py
@@ -14,7 +14,7 @@ class Player(db.Model, ModelMixin):
   level = db.Column(db.SmallInteger(unsigned=True), nullable=False)
   house = db.Column(db.Boolean, nullable=False)
 
-  game = db.relationship('Game', backref=db.backref('players', lazy='dynamic'))
+  game = db.relationship('Game', foreign_keys=game_id, backref=db.backref('players', lazy='dynamic'))
   user = db.relationship('User', backref=db.backref('players', lazy='dynamic'))
 
   def __init__(self, game, user):
