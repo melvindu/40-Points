0b170658341df3bbf3c14b6c434f3ac61e5c3d09
Melvin Du <melvinclanceydu@gmail.com>
2013-12-30 21:40:46 -0800

    readding removed stuff

--- fortypoints/games/views.py
+++ fortypoints/games/views.py
@@ -2,11 +2,21 @@ from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import login_required
 
 from fortypoints.template import templated
-from fortypoints import games
+from fortypoints.games import create_game
+from fortypoints.users import get_user
 from fortypoints.games.forms import NewGameForm
 
 game = Blueprint('games', __name__, template_folder='templates/games')
 
+
+@game.route('/play/<int:game_id>', methods=['GET', 'POST'])
+@login_required
+def play(game_id):
+  """
+  Play a game.
+  """
+  return render_template('games/play.html')
+  
 
 @game.route('/new', methods=['GET', 'POST'])
 @login_required
@@ -21,5 +31,9 @@ def new():
       form.players.append_entry()
       return render_template('games/new.html', form=form)
     else:
-      pass
+      users = filter(None, form.players.entries.data)
+      users = [get_user(user) for user in users]
+      game = create_game(users)
+      return redirect(url_for('games.'))
+  flash('Invalid User', 'error')
   return render_template('games/new.html', form=form)
