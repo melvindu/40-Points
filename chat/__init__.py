05ad47e83fea8002559ea408cfa75eb28b99dd78
Melvin Du <melvinclanceydu@gmail.com>
2014-01-01 15:36:18 -0800

    removed tornado stuff

--- chat/__init__.py
+++ chat/__init__.py
@@ -2,75 +2,9 @@ import logging
 import os.path
 import uuid
 
+from flask import current_app, request
 from flask.ext.login import current_user, login_required
-import tornado.escape
-import tornado.ioloop
-import tornado.options
-from tornado.options import define, options
-import tornado.web
-import tornado.websocket
 
 import fortypoints as fp
 from fortypoints import app
-from fortypoints.request import flask_context
-
-
-class ChatApplication(tornado.web.Application):
-    def __init__(self):
-        handlers = [
-            (r"/gamechat", ChatSocketHandler),
-        ]
-        settings = dict(
-            cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
-            template_path=os.path.join(os.path.dirname(__file__), "templates"),
-            static_path=os.path.join(os.path.dirname(__file__), "static"),
-            xsrf_cookies=True,
-        )
-        tornado.web.Application.__init__(self, handlers, **settings)
-
-
-class ChatSocketHandler(tornado.websocket.WebSocketHandler):
-    waiters = set()
-    cache = []
-    cache_size = 200
-
-    def allow_draft76(self):
-        # for iOS 5.0 Safari
-        return True
-
-    @flask_context(fp.app)
-    @login_required
-    def open(self):
-        print 'OPENED'
-        ChatSocketHandler.waiters.add(self)
-
-    def on_close(self):
-        ChatSocketHandler.waiters.remove(self)
-
-    @classmethod
-    def update_cache(cls, chat):
-        cls.cache.append(chat)
-        if len(cls.cache) > cls.cache_size:
-            cls.cache = cls.cache[-cls.cache_size:]
-
-    @classmethod
-    def send_updates(cls, chat):
-        logging.info("sending message to %d waiters", len(cls.waiters))
-        for waiter in cls.waiters:
-            try:
-                waiter.write_message(chat)
-            except:
-                logging.error("Error sending message", exc_info=True)
-
-    def on_message(self, message):
-        logging.info("got message %r", message)
-        parsed = tornado.escape.json_decode(message)
-        chat = {
-            "id": str(uuid.uuid4()),
-            "body": parsed["body"],
-            }
-        chat["html"] = tornado.escape.to_basestring(
-            self.render_string("message.html", message=chat))
-
-        ChatSocketHandler.update_cache(chat)
-        ChatSocketHandler.send_updates(chat)
+from fortypoints.request import flask_context, websocket
