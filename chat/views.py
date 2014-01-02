1e0d4f208f96b909ec3e21be6f946bdb622ddff1
Melvin Du <melvinclanceydu@gmail.com>
2014-01-01 16:07:42 -0800

    stuff

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,5 @@
+import simplejson as json
+
 from flask import Blueprint
 
 from fortypoints.games.views import game
@@ -10,11 +12,15 @@ chat = Blueprint('chats', __name__)
 def handle_websocket(ws, game_id):
   while True:
     message = ws.receive()
+    print message
     if message is None:
+      print 'broke'
       break
     else:
-      message = json.loads(message)
+      #message = json.loads(message)
 
       r  = "I have received this message from you : %s" % message
       r += "<br>Glad to be your webserver."
+      print 'sending'
       ws.send(json.dumps({'output': r}))
+      print 'sent'
