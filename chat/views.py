265f0984194859e2b4bd8982d42f809faa358c85
Melvin Du <melvinclanceydu@gmail.com>
2014-01-01 20:11:31 -0800

    backbone in base

--- chat/views.py
+++ chat/views.py
@@ -3,12 +3,14 @@ import simplejson as json
 from flask import Blueprint
 
 from fortypoints.games.views import game
+from fortypoints.players.decorators import player_required
 from fortypoints.request import websocket
 
 chat = Blueprint('chats', __name__)
 
 
 @websocket(chat, '/game/<int:game_id>')
+@player_required
 def game_chat_handler(ws, game_id):
   while True:
     message = ws.receive()
