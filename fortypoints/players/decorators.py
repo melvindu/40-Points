7e9c1e6e391dfbbda3dc1b35691b0a267b6e6424
Melvin Du <melvinclanceydu@gmail.com>
2014-01-03 15:52:42 -0800

    player_id support for player_required

--- fortypoints/players/decorators.py
+++ fortypoints/players/decorators.py
@@ -4,18 +4,29 @@ from flask import g, redirect, url_for
 from flask.ext.login import current_user, login_required
 
 from fortypoints.games import get_game
-from fortypoints.players import get_player
+from fortypoints.players import get_player, get_player_by_id
 
 def player_required(func):
   @wraps(func)
   def wrapper(*args, **kwargs):
-    game_id = kwargs['game_id']
-    game = get_game(game_id)
     if not current_user:
       return login_required(func)(*args, **kwargs)
-    player = get_player(game, current_user)
-    if player:
-      return login_required(func)(*args, **kwargs)
+
+    player_id = kwargs.get('player_id')
+    game_id = kwargs.get('game_id')
+    if player_id:
+      player = get_player_by_id(player_id)
+      if player.user.id == current_user.id:
+        return login_required(func)(*args, **kwargs)
+      else:
+        return redirect(url_for('index'))
+    elif game_id:
+      game = get_game(game_id)
+      player = get_player(game, current_user)
+      if player:
+        return login_required(func)(*args, **kwargs)
+      else:
+        return redirect(url_for('index'))
     else:
       return redirect(url_for('index'))
   return wrapper
