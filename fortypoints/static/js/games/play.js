b3e7e38953392834c76bd50075e1fdea0dda1a60
Melvin Du <melvinclanceydu@gmail.com>
2014-01-02 16:50:02 -0800

    event tirggers

--- fortypoints/static/js/games/play.js
+++ fortypoints/static/js/games/play.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
   var chatView = new CHAT.ChatView()
+  var views = [chatView]
   var gameUpdater = new WebSocket('ws://' + location.host + '/game/update-stream/' + GAME_ID)
-  console.log('foo')
+
   gameUpdater.onmessage = function(message) {
-    console.log(message);
+    for (index in views) {
+      views[index].trigger(message.data.event);
+    }
+  };
+
+  window.onbeforeunload = function() {
+    gameUpdater.close()
   };
 });
