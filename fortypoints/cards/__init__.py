69367128ab9cb174816fa69c36a0bbfc154242a9
Melvin Du <melvinclanceydu@gmail.com>
2014-01-03 00:33:51 -0800

    flip class

--- fortypoints/cards/__init__.py
+++ fortypoints/cards/__init__.py
@@ -7,6 +7,74 @@ from fortypoints.cards.models import Card as CardModel, CardMixin as Card
 db = fp.db
 
 
+class Flip(object):
+  def __init__(self, game, cards):
+    # check unflippable
+    flippable_nums = (game.trump_number, CARD.SMALL_JOKER, CARD.BIG_JOKER)
+    if not all(card.num in flippable_nums for card in cards):
+      raise ValueError('Can\'t flip non-level card')
+    # check nonequal cards
+    if len(cards) > 1:
+      if len(set(cards)) > 1:
+        raise ValueError('Can\'t flip nonequivalent cards')
+    self.game = game
+    self.cards = cards
+
+  def __eq__(self, other):
+    self.cards == other.cards
+
+  def __lt__(self, other):
+    return not (self == other) and len(self) < len(other)
+
+  def __gt__(self, other):
+    return not self == other and len(self) > len(other)
+
+
+class GameCard(Card):
+  def __init__(self, game, card):
+    self.game = game
+    Card.__init__(card.num, card.suit)
+
+  @property
+  def trump_number(self):
+    return self.game.trump_number
+
+  @property
+  def trump_suit(self):
+    return self.game.trump_number
+
+  @property
+  def is_trump(self):
+    return self.num == self.trump_number or \
+           self.suit == self.trump_suit or \
+           self.num in CARD.JOKERS
+
+  def __repr__(self):
+    return '<GameCard \'{name}\'>'.format(name=self.name)
+    
+  def __eq__(self, other):
+    return self.suit == other.suit and self.num == other.num
+
+  def __lt__(self, other):
+    if self.is_trump and other.is_trump:
+      return self.num < other.num
+    elif self.is_trump and not other.is_trump:
+      return False
+    elif not self.is_trump and other.is_trump:
+      return True
+    else:
+      return False
+
+  def __gt__(self, other):
+    if self.is_trump and other.is_trump:
+      return self.num > other.num
+    elif self.is_trump and not other.is_trump:
+      return True
+    elif not self.is_trump and other.is_trump:
+      return False
+    else:
+      return False
+
 class Deck(object):
   def __init__(self, size=1):
     deck = []
