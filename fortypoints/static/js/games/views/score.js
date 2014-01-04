8e2ab31431f9c5e71ef4b27ef048c5d81f035c6c
Melvin Du <melvinclanceydu@gmail.com>
2014-01-03 17:57:30 -0800

    backbone works omg

--- fortypoints/static/js/games/views/score.js
+++ fortypoints/static/js/games/views/score.js
@@ -9,11 +9,30 @@ var ScoreView = Backbone.View.extend({
         this.model.on('change:score', this.render);
     },
     render: function() {
-        this.$el.html('<b>' + this.model.get('name') + '</b>: ' + this.model.score);
-        return this;
+      this.$el.html('<b>' + this.model.get('name') + '</b>: ' + this.model.score);
+      return this;
     }
 });
 
+var ScoreBoardView = Backbone.View.extend({
+  initialize: function() {
+    _.bindAll(this, 'render');
+    _.bindAll(this, 'add');
+    this.collection.on('add', this.add);
+  },
+  render: function() {
+    return this;
+  },
+
+  add: function(model) {
+    var scoreView = new ScoreView({model: model});
+    this.$el.append(scoreView.el);
+    scoreView.render()
+    return this;
+  }
+})
+
   mod.ScoreView = ScoreView;
+  mod.ScoreBoardView = ScoreBoardView;
   return mod;
 }());
