8f72310ca75efaf02d11d8e55fc05ea36e282635
Melvin Du <melvinclanceydu@gmail.com>
2014-01-04 00:27:41 -0800

    current player styling

--- fortypoints/static/js/games/models/player.js
+++ fortypoints/static/js/games/models/player.js
@@ -23,7 +23,29 @@ PLAYER = (function() {
       } else {
         label = 'danger'
       }
-      this.$el.html('<h2><span class="label label-' + label + '">' + this.model.get('name') + '</span></h2>');
+      if (this.model.get('name')){
+        this.$el.html('<h2><span class="label label-' + label + '">' + this.model.get('name') + '</span></h2>');
+      }
+      return this;
+    }
+  });
+
+  var CurrentPlayerView = Backbone.View.extend({
+    el: '.current-player',
+    tagName: 'div',
+    initialize: function() {
+      _.bindAll(this, 'render');
+      this.collection.on('change:active', this.render);
+    },
+
+    render: function() {
+      var view = this;
+      this.collection.each(function(player) {
+        if (player.get('active')) {
+          console.log("IM ACTIVE!")
+          view.$el.html('<span class="label label-info current-player">Turn: ' + player.get('name') + '</span>');
+        };
+      });
       return this;
     }
   });
@@ -32,5 +54,6 @@ PLAYER = (function() {
   mod.Player = Player;
   mod.Players = Players;
   mod.PlayerStatusView = PlayerStatusView;
+  mod.CurrentPlayerView = CurrentPlayerView;
   return mod;
 }())
