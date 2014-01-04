2f518d6f822d83ffe3344dd0f22f184d0da9e092
Melvin Du <melvinclanceydu@gmail.com>
2014-01-03 23:30:35 -0800

    pro active design, action button

--- fortypoints/static/js/games/models/player.js
+++ fortypoints/static/js/games/models/player.js
@@ -10,8 +10,27 @@ PLAYER = (function() {
     model: Player
   });
 
+  var PlayerStatusView = Backbone.View.extend({
+    tagName: 'span',
+    initialize: function() {
+      _.bindAll(this, 'render');
+      this.model.on('change:active', this.render);
+    },
+    render: function() {
+      var label = ''
+      if (this.model.get('active')) {
+        label = 'success'
+      } else {
+        label = 'danger'
+      }
+      this.$el.html('<h2><span class="label label-' + label + '">' + this.model.get('name') + '</span></h2>');
+      return this;
+    }
+  });
+
   var mod = {};
   mod.Player = Player;
   mod.Players = Players;
+  mod.PlayerStatusView = PlayerStatusView;
   return mod;
 }())
