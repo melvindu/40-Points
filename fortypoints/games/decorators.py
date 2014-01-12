b7c5533460d16aede0130354710aa3ec10d1ffe1
melvindu <melvinclanceydu@gmail.com>
2014-01-12 15:28:03 -0800

    GameError and requires decorator

--- fortypoints/games/decorators.py
+++ fortypoints/games/decorators.py
@@ -4,7 +4,9 @@ import logging
 from flask import g, jsonify, redirect, url_for
 from flask.ext.login import current_user, login_required
 
+from fortypoints.cards.decorators import get_cards_from_form
 from fortypoints.games import get_game
+from fortypoints.games.exceptions import GameError
 from fortypoints.games.updates import update_game_client
 from fortypoints.players import get_player, get_player_by_id
 
@@ -47,4 +49,48 @@ def game_response(updates):
         logging.exception(e)
         return jsonify({'status': False, 'data': e.message})
     return decorator
+  return wrapper
+
+def requires(*args):
+  @wraps(func)
+  def wrapper(*args, **kwargs):
+    if not current_user:
+      return login_required(func)(*args, **kwargs)
+    player_id = kwargs.get('player_id')
+    game_id = kwargs.get('game_id')
+
+    #prioritize player id
+    if player_id:
+      req_player = get_player_by_id(player_id)
+      game = player.game
+    elif game_id:
+      game = get_game(game_id)
+    
+    curr_player = get_player(game, current_user)
+
+    if 'game' in args:
+      if not curr_player:
+        return redirect(url_for('index'))
+
+    if 'player' in args:
+      if req_player.user.id != current_user.id:
+        return redirect(url_for('index'))
+
+    if 'cards' in args:
+      cards = get_cards_from_form(request.form)
+      curr_player.get_cards(cards)
+
+    if 'active' in args:
+      if not curr_player.active:
+        raise GameError('You are not the currently active player')
+
+    if 'lead' in args:
+      if not curr_player.lead:
+        raise GameError('You are not the game lead.')
+
+    if 'house' in args:
+      if not curr_player.house:
+        raise GameError('You are not on the house team.')
+
+    return login_required(func)(*args, **kwargs)
   return wrapper
