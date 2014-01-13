162f5b94dea4f56dbd85904d7598aede841e0c88
melvindu <melvinclanceydu@gmail.com>
2014-01-12 22:20:43 -0800

    view for plays

--- fortypoints/static/js/games/models/player.js
+++ fortypoints/static/js/games/models/player.js
@@ -48,10 +48,38 @@ PLAYER = (function() {
     }
   });
 
+  var PlaysView = Backbone.View.extend({
+     initialize: function() {
+      _.bindAll(this, 'render');
+      this.collection.on('change:play', this.render);
+    },
+    render: function() {
+      var view = this;
+      this.collection.each(function(player) {
+        var view = $('.play[player_id="' + player.id + '"');
+        var cards = []
+        var src = ''
+        var card = {}
+        var card_img = ''
+        for (var index in player.get('play')) {
+          card = player.get('play')[index]
+          card_img = String(card.num) + String(card.suit) + '.jpg'
+          src = location.origin + '/static/images/' + card_img
+          cards.push('<li class="card" num="' + card.num + '" suit="' + card.suit + '">' + 
+            '<img src="' + src + '" alt="' + card_img + '" class="img-thumbnail card"></li>');
+          $(view).html('<li class="card" num="' + card.num + '" suit="' + card.suit + '">' + 
+            '<img src="' + src + '" alt="' + card_img + '" class="img-thumbnail card"></li>')
+        }
+      });
+      return this;
+    }
+  });
+
   var mod = {};
   mod.Player = Player;
   mod.Players = Players;
   mod.PlayerStatusView = PlayerStatusView;
   mod.CurrentPlayerView = CurrentPlayerView;
+  mod.PlaysView = PlaysView;
   return mod;
 }())
